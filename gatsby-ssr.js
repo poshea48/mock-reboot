2c956794f09301d75353c810a18d91d9b5083137
Paul O'Shea <poshea48@msn.com>
2020-02-23 12:07:23 -1000

    gatsby-browser/ssr change to just wrapRootElement

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,18 +3,22 @@ import MainLayout from './src/components/layout/MainLayout';
 import PropTypes from 'prop-types';
 import { AppProvider } from './src/context/appContext';
 
-export const wrapPageElement = ({ element, props }) => {
-  return <MainLayout {...props}>{element}</MainLayout>;
-};
-
 export const wrapRootElement = ({ element, props }) => {
-  return <AppProvider {...props}>{element}</AppProvider>;
+  return (
+    <MainLayout {...props}>
+      <AppProvider {...props}>{element}</AppProvider>
+    </MainLayout>
+  );
 };
 
-wrapPageElement.propTypes = {
-  props: PropTypes.object,
-  element: PropTypes.func,
-};
+// export const wrapRootElement = ({ element, props }) => {
+//   return <AppProvider {...props}>{element}</AppProvider>;
+// };
+
+// wrapPageElement.propTypes = {
+//   props: PropTypes.object,
+//   element: PropTypes.func,
+// };
 
 wrapRootElement.propTypes = {
   props: PropTypes.object,
